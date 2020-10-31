b87b21973f4b5005357900844436f13e019fe159
JeongHyeon <jhjhjh96@huve.co.kr>
2020-10-31 15:38:17 +0900

    API config 파일 수정

--- app/src/main/java/kr/co/huve/wealth/model/backend/NetworkConfig.kt
+++ app/src/main/java/kr/co/huve/wealth/model/backend/NetworkConfig.kt
@@ -1,10 +1,14 @@
 package kr.co.huve.wealth.model.backend
 
-import javax.inject.Singleton
-
-@Singleton
 object NetworkConfig {
-    val WEATHER_API = "https://api.openweathermap.org/data/2.5/"
+
+    //region Common setting
+    private const val CONNECT_TIMEOUT = 10L
+    private const val WRITE_TIMEOUT = 1L
+    private const val READ_TIMEOUT = 20L
+    //endregion Common setting
+
+    const val WEATHER_API = "https://api.openweathermap.org/data/2.5/"
     // @Todo: Insert your Api key from https://openweathermap.org/. The api key is free.
-    val WEATHER_KEY = ""
+    private const val WEATHER_KEY = ""
 }
