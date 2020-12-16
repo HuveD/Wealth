7b487ee801e8c5c2f26634cfc235d3d8e437b611
JeongHyeon <jhjhjh96@huve.co.kr>
2020-12-16 23:03:29 +0900

    차트 기능 테스트위한 샘플 액티비티 추가

---
+++ app/src/main/java/kr/co/huve/wealthApp/view/ChartTestActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.huve.wealthApp.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.huve.wealthApp.R
+
+class ChartTestActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.chart_test_activty)
+    }
+}
